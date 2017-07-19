4ea414fdd0505b0cf42ddaca23251b91572bbd44
Daniel Tapia <danieltapia@daniels-mbp.attlocal.net>
2017-07-18 21:21:42 -0500

    Added stick navbar

--- src/js/main.js
+++ src/js/main.js
@@ -4,149 +4,179 @@
             focusOnSelect: true,
             variableWidth: true,     */
 $(document).ready(function(){
-        $('.engagementPictures').slick({
+
+  //Stick navbar on scroll
+  var stickyToggle = function(sticky, stickyWrapper, scrollElement) {
+  var stickyHeight = sticky.outerHeight();
+  var stickyTop = stickyWrapper.offset().top;
+  if (scrollElement.scrollTop() >= stickyTop){
+   stickyWrapper.height(stickyHeight);
+   sticky.addClass("is-sticky");
+  } else{
+     sticky.removeClass("is-sticky");
+     stickyWrapper.height('auto');
+    }
+  };
+
+  // Find all data-toggle="sticky-onscroll" elements
+  $('[data-toggle="sticky-onscroll"]').each(function() {
+    var sticky = $(this);
+    var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
+    sticky.before(stickyWrapper);
+    sticky.addClass('sticky');
+
+    // Scroll & resize events
+    $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function() {
+     stickyToggle(sticky, stickyWrapper, $(this));
+  });
+
+  // On page load
+  stickyToggle(sticky, stickyWrapper, $(window));
+  });
+
+  $('.engagementPictures').slick({
+      centerMode: true,
+      slidesToShow: 3,
+      dots: true,
+      speed: 300,
+      centerPadding: '0px',
+      responsive: [
+        {
+        breakpoint: 480,
+          settings: {
             centerMode: true,
-            slidesToShow: 3,
-            dots: true,
-            speed: 300,
+            slidesToShow: 1,
+            centerPadding: '0'
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
             centerPadding: '0px',
-            responsive: [
-              {
-              breakpoint: 480,
-                settings: {
-                  centerMode: true,
-                  slidesToShow: 1,
-                  centerPadding: '0'
-                }
-              },
-              {
-                breakpoint: 768,
-                settings: {
-                  arrows: false,
-                  centerMode: true,
-                  centerPadding: '0px',
-                  slidesToShow: 2
-                }
-              }
-            ]
-            /*responsive: [
-              {
-              variableWidth: true,
-                breakpoint: 768,
-                settings: {
-                  arrows: false,
-                  centerMode: true,
-                  centerPadding: '40px',
-                  slidesToShow: 3
-                }
-              },
-              {
-              variableWidth: true,
-                breakpoint: 480,
-                settings: {
-                  arrows: false,
-                  centerMode: true,
-                  centerPadding: '40px',
-                  slidesToShow: 3
-                }
-              }
-            ]   */
-        });
+            slidesToShow: 2
+          }
+        }
+      ]
+      /*responsive: [
+        {
+        variableWidth: true,
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+        variableWidth: true,
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        }
+      ]   */
+  });
 
-        var $images = $('.Collage');
+  var $images = $('.Collage');
 
-        $('.Collage').collagePlus({
-                'targetHeight' : 130,
-                'allowPartialLastRow' : true
-            }
-        );
+  $('.Collage').collagePlus({
+          'targetHeight' : 130,
+          'allowPartialLastRow' : true
+      }
+  );
 
-      $('img').hover(function(){
-        var yoffset = $('#wedding-party').height();
-        var height = $(this).height();
-        var width = $(this).width();
-        var coords = $(this).offset();
-        $('#caption').css('line-height', height + 'px');
-        $('#caption').html($(this).data('caption'))
-            .width(width+2)
-            .height(height+2)
-            .animate({
-                top: coords.top - (yoffset*4 -3),
-                left: coords.left -2
-            }, 0, function(){
-                $(this).animate({
-                    opacity: 'show'
-                });
-            });
+$('img').hover(function(){
+  var yoffset = $('#wedding-party').height();
+  var height = $(this).height();
+  var width = $(this).width();
+  var coords = $(this).offset();
+  $('#caption').css('line-height', height + 'px');
+  $('#caption').html($(this).data('caption'))
+      .width(width+2)
+      .height(height+2)
+      .animate({
+          top: coords.top - (yoffset*4 -3),
+          left: coords.left -2
+      }, 0, function(){
+          $(this).animate({
+              opacity: 'show'
+          });
       });
+});
 
-      $('img').mouseout(function(){
-          $('#caption').animate({
-              opacity: 'hide'
-          }, 100);
-      });
+$('img').mouseout(function(){
+    $('#caption').animate({
+        opacity: 'hide'
+    }, 100);
+});
 
-        var $weddingParty = $('#wedding-party');
-        var $infoClose = $('.infoClose');
-        var showInfo = false;
-        var friendsInfo = false;
-        var currInfo = '';
-        var $timeline = $('#carousel-inner');
+  var $weddingParty = $('#wedding-party');
+  var $infoClose = $('.infoClose');
+  var showInfo = false;
+  var friendsInfo = false;
+  var currInfo = '';
+  var $timeline = $('#carousel-inner');
 
-        $timeline.bind('click', function(event){
-          event.preventDefault();
-          if(!showInfo){
-            var href = event.target.getAttribute('href');
-            href = href.replace('#','');
-            currInfo = document.getElementById(href);
-            currInfo.classList.add('show-info');
-            showInfo = true;
-          }
-          else{
-            var currClass = event.target.getAttribute('class');
-            if(currClass.includes('info-close')){
-              currInfo.classList.remove('show-info');
-              currInfo.classList.add('hide-info');
+  $timeline.bind('click', function(event){
+    event.preventDefault();
+    if(!showInfo){
+      var href = event.target.getAttribute('href');
+      href = href.replace('#','');
+      currInfo = document.getElementById(href);
+      currInfo.classList.add('show-info');
+      showInfo = true;
+    }
+    else{
+      var currClass = event.target.getAttribute('class');
+      if(currClass.includes('info-close')){
+        currInfo.classList.remove('show-info');
+        currInfo.classList.add('hide-info');
 
-              setTimeout(function(){
-                currInfo.classList.remove('hide-info');
-              },2000);
+        setTimeout(function(){
+          currInfo.classList.remove('hide-info');
+        },2000);
 
-              showInfo=false;
-            }
-          }
-        });
+        showInfo=false;
+      }
+    }
+  });
 
-        $weddingParty.bind('click', function(event){
-          event.preventDefault();
-          if(!friendsInfo){
-            var href = event.target.getAttribute('href');
-            href = href.replace('#','');
-            currInfo = document.getElementById(href);
-            currInfo.classList.add('show-info');
-            friendsInfo = true;
-          }
-          else{
-            var currClass = event.target.getAttribute('class');
-            if(currClass.includes('info-close')){
-              currInfo.classList.remove('show-info');
-              currInfo.classList.add('hide-info');
+  $weddingParty.bind('click', function(event){
+    event.preventDefault();
+    if(!friendsInfo){
+      var href = event.target.getAttribute('href');
+      href = href.replace('#','');
+      currInfo = document.getElementById(href);
+      currInfo.classList.add('show-info');
+      friendsInfo = true;
+    }
+    else{
+      var currClass = event.target.getAttribute('class');
+      if(currClass.includes('info-close')){
+        currInfo.classList.remove('show-info');
+        currInfo.classList.add('hide-info');
 
-              setTimeout(function(){
-                currInfo.classList.remove('hide-info');
-              },2000);
+        setTimeout(function(){
+          currInfo.classList.remove('hide-info');
+        },2000);
 
-              friendsInfo=false;
-            }
-          }
-        });
-      });
+        friendsInfo=false;
+      }
+    }
+  });
+});
 
-    var viewInfoButton = document.getElementsByClassName("history-more");
-    var info = document.getElementsByClassName("info");
-    $('.carousel').carousel({
-      interval: false
-    });
+  var viewInfoButton = document.getElementsByClassName("history-more");
+  var info = document.getElementsByClassName("info");
+  $('.carousel').carousel({
+    interval: false
+});
 
     // Init Skrollr
     // var s = skrollr.init();
